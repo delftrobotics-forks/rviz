492992b4cd836d56039b53be610ae272c1c6d02f
Maarten de Vries <maarten@de-vri.es>
2020-12-29 14:19:20 +0100

    Fix setting material of point clouds.

--- src/rviz/ogre_helpers/point_cloud.cpp
+++ src/rviz/ogre_helpers/point_cloud.cpp
@@ -299,7 +299,7 @@ void PointCloud::setRenderMode(RenderMode mode)
   V_PointCloudRenderable::iterator end = renderables_.end();
   for (; it != end; ++it)
   {
-    (*it)->setMaterial(current_material_->getName());
+    (*it)->setMaterial(current_material_);
   }
 
   regenerateAll();
@@ -731,7 +731,7 @@ PointCloudRenderablePtr PointCloud::createRenderable(int num_points)
 {
   PointCloudRenderablePtr rend(
       new PointCloudRenderable(this, num_points, !current_mode_supports_geometry_shader_));
-  rend->setMaterial(current_material_->getName());
+  rend->setMaterial(current_material_);
   Ogre::Vector4 size(width_, height_, depth_, 0.0f);
   Ogre::Vector4 alpha(alpha_, 0.0f, 0.0f, 0.0f);
   Ogre::Vector4 highlight(0.0f, 0.0f, 0.0f, 0.0f);
